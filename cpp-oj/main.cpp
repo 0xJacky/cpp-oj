cb9106ed48095865d7da8ee38162c8ff0c0e08dc
Jacky <jacky-943572677@qq.com>
2020-09-09 22:47:06 +0800

    Initial Commit

---
+++ cpp-oj/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp-oj
+//
+//  Created by Jacky on 2020/9/9.
+//  Copyright © 2020 Jacky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
